e1951fe975584496c212040587bde7e9b5e063be
sakshi-rumsan <sakshi.nepal@rumsan.net>
2026-05-01 11:46:12 +0545

    modified docke r compose file

--- infra/alembic/versions/20260422_create_api_keys_table.py
+++ infra/alembic/versions/20260422_create_api_keys_table.py
@@ -1,8 +1,15 @@
 """
 Alembic migration for api_keys table
 """
+
 from alembic import op
 import sqlalchemy as sa
+
+# Revision identifiers, used by Alembic.
+revision = '20260422_create_api_keys_table'
+down_revision = None
+branch_labels = None
+depends_on = None
 
 def upgrade():
     op.create_table(
